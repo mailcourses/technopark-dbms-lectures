16fcc5f1e1d14bb645eceddf6c8715de7f179980
Nikita Strelkov <n.strelkov@corp.mail.ru>
2021-10-15 17:06:36 +0300

    docker

---
+++ scripts/docker/postgres/init/create_extensions.sql
@@ -0,0 +1,5 @@
+set search_path = public;
+create extension if not exists moddatetime;
+create extension if not exists ltree;
+create extension if not exists pgcrypto;
+create extension if not exists "uuid-ossp";
