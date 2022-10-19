497af870a144dd08f4295ce234d6e95817a25bda
Nikita Strelkov <nikita.strelkov@gmail.com>
2022-10-19 18:40:48 +0400

    add auto_explain

--- scripts/movielens/load-pgsql.sh
+++ scripts/movielens/load-pgsql.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 DB_USER=$1
-DB_PASSWORD=$2
 DB=movielens
 set -e -x
-psql "postgres://$DB_USER:$DB_PASSWORD@localhost:5432/postgres?sslmode=disable"  -c "drop database if exists \"$DB\";"
-psql "postgres://$DB_USER:$DB_PASSWORD@localhost:5432/postgres?sslmode=disable"  -c "create database \"$DB\";"
+psql "postgres://$DB_USER@localhost:5432/postgres?sslmode=disable"  -c "drop database if exists \"$DB\";"
+psql "postgres://$DB_USER@localhost:5432/postgres?sslmode=disable"  -c "create database \"$DB\";"
 wget -c http://files.grouplens.org/datasets/movielens/ml-latest.zip
 unzip -o ml-latest.zip
-psql "postgres://$DB_USER:$DB_PASSWORD@localhost:5432/$DB?sslmode=disable" < load-pgsql.sql
+psql "postgres://$DB_USER@localhost:5432/$DB?sslmode=disable" < load-pgsql.sql
 pg_dump $DB > ml-latest.sql
