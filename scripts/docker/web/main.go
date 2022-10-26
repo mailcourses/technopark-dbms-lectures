32a6d751b2ec5722d47eaf9f194ddbcb3b882fff
Nikita Strelkov <nikita.strelkov@gmail.com>
2022-10-26 17:54:03 +0400

    Some updates

--- scripts/docker/web/main.go
+++ scripts/docker/web/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "URL: "+r.URL.String())
+}
 
 func main() {
+	mux := http.NewServeMux()
+
+	// Register routes and register handlers in this form.
+	mux.Handle("/", &myHandler{})
+
+	//http.ListenAndServe uses the default server structure.
+	err := http.ListenAndServe(":8090", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
