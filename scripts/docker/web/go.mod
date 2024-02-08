55f3a97c97828d9a68fe5612b0f525a031dfe4a1
Nikita Strelkov <nikita.strelkov@gmail.com>
2024-02-08 17:07:26 +0200

    Updates

---
+++ scripts/docker/web/go.mod
@@ -0,0 +1,16 @@
+module github.com/technopark-dbms-lectures/scripts/docker/web
+
+go 1.21.4
+
+require github.com/jackc/pgx v3.6.2+incompatible
+
+require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- scripts/docker/web/main.go
+++ scripts/docker/web/main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
-	"io"
-	"log"
+	"fmt"
 	"net/http"
+
+	"github.com/jackc/pgx"
 )
 
-type myHandler struct{}
+var conn *pgx.Conn
 
-func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "URL: "+r.URL.String())
+func init() {
+	var err error
+	conn, err = pgx.Connect(
+		pgx.ConnConfig{
+			Host:     "localhost",
+			Port:     6432,
+			User:     "techdb",
+			Password: "techdb",
+			Database: "techdb",
+		},
+	)
+	if err != nil {
+		fmt.Printf("Unable to connection to database: %v\n", err)
+		return
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	rows, err := conn.Query("select * from checkpoint_highschooler.highschooler")
+	if err != nil {
+		fmt.Fprintf(w, "Error querying the database: %v\n", err)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var grade int
+		err = rows.Scan(&id, &name, &grade)
+		if err != nil {
+			fmt.Fprintf(w, "Error scanning the row: %v\n", err)
+			return
+		}
+		fmt.Fprintf(w, "ID: %d, Name: %s, Grade: %d\n", id, name, grade)
+	}
 }
 
 func main() {
-	mux := http.NewServeMux()
-
-	// Register routes and register handlers in this form.
-	mux.Handle("/", &myHandler{})
-
-	//http.ListenAndServe uses the default server structure.
-	err := http.ListenAndServe(":8090", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
